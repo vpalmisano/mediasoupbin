a0987aeb537eb7ade6d649bc38c9c7cbaa98d60a
Vittorio Palmisano <vpalmisano@gmail.com>
2019-12-13 14:47:13 +0100

    code cleanup

--- gst/plugins/python/lib/pipelines.py
+++ gst/plugins/python/lib/pipelines.py
@@ -17,46 +17,109 @@ def setEncoderDesc(config, level=''):
         config['token_partitions'] = min(math.ceil(math.sqrt(config['cpu_count'])), 8)
         # hardware encoder
         if config['hw'] == 'vaapi' and (not level or level == '_2'):
-            config['video_encoder_desc' + level] = 'vaapi%(codec)senc bitrate=%(bitrate)d rate-control=cbr \
-keyframe-period=%(max_keyframe_distance)d quality-level=7' %config
+            config['video_encoder_desc' + level] = 'vaapi%(codec)senc \
+                bitrate=%(bitrate)d \
+                rate-control=cbr \
+                keyframe-period=%(max_keyframe_distance)d \
+                quality-level=7' %config
         # software encoder
         else:
-            config['video_encoder_desc' + level] = '''%(codec)senc lag-in-frames=0 error-resilient=default \
-deadline=1 cpu-used=-5 end-usage=cbr target-bitrate=%(bitrate)d000 min-quantizer=5 max-quantizer=56 undershoot=100 overshoot=15 \
-dropframe-threshold=50 resize-allowed=false \
-buffer-initial-size=500 buffer-optimal-size=600 buffer-size=1000 max-intra-bitrate=100 \
-keyframe-mode=auto keyframe-max-dist=%(max_keyframe_distance)d \
-token-partitions=%(token_partitions)d threads=%(cpu_count)d static-threshold=1 noise-sensitivity=1''' %config
+            config['video_encoder_desc' + level] = '''%(codec)senc \
+                lag-in-frames=0 \
+                error-resilient=default \
+                deadline=1 \
+                cpu-used=-5 \
+                end-usage=cbr \
+                target-bitrate=%(bitrate)d000 \
+                min-quantizer=5 \
+                max-quantizer=56 \
+                undershoot=100 \
+                overshoot=15 \
+                dropframe-threshold=50 \
+                resize-allowed=false \
+                buffer-initial-size=500 \
+                buffer-optimal-size=600 \
+                buffer-size=1000 \
+                max-intra-bitrate=100 \
+                keyframe-mode=auto \
+                keyframe-max-dist=%(max_keyframe_distance)d \
+                token-partitions=%(token_partitions)d \
+                threads=%(cpu_count)d \
+                static-threshold=1 \
+                noise-sensitivity=1''' %config
     # h264
     else:
         config['video_rtppay_options'] = ''
         # vaapi hardware encoder
         if config['hw'] == 'vaapi' and (not level or level == '_2'):
-            config['video_encoder_desc' + level] = 'vaapi%(codec)senc bitrate=%(bitrate)d rate-control=cbr \
-keyframe-period=%(max_keyframe_distance)d quality-level=7 ! video/x-%(codec)s,profile=high' %config
+            config['video_encoder_desc' + level] = 'vaapi%(codec)senc \
+                bitrate=%(bitrate)d \
+                rate-control=cbr \
+                keyframe-period=%(max_keyframe_distance)d \
+                quality-level=7 \
+                ! video/x-%(codec)s,profile=high' %config
         # nv hardware encoder
         elif config['hw'] == 'nv': # and (not level or level == '_2'):
-            config['video_encoder_desc' + level] = 'nvh264enc preset=low-latency bitrate=%(bitrate)d rc-mode=cbr \
-gop-size=%(max_keyframe_distance)d ! h264parse ! video/x-h264,stream-format=avc' %config
+            config['video_encoder_desc' + level] = 'nvh264enc \
+                preset=low-latency \
+                bitrate=%(bitrate)d \
+                rc-mode=cbr \
+                gop-size=%(max_keyframe_distance)d \
+                ! h264parse \
+                ! video/x-h264,stream-format=avc' %config
         # software encoder
         elif config['codec'] == 'h264':
-            config['video_encoder_desc' + level] = 'x264enc pass=cbr bitrate=%(bitrate)d \
-key-int-max=%(max_keyframe_distance)d speed-preset=veryfast tune=zerolatency vbv-buf-capacity=0 ! video/x-h264,profile=high' %config #option-string="no-sliced-threads"
+            config['video_encoder_desc' + level] = 'x264enc \
+                pass=cbr \
+                bitrate=%(bitrate)d \
+                key-int-max=%(max_keyframe_distance)d \
+                speed-preset=veryfast \
+                tune=zerolatency \
+                vbv-buf-capacity=0 \
+                ! video/x-h264,profile=high' %config #option-string="no-sliced-threads"
         elif config['codec'] == 'h265':
-            config['video_encoder_desc' + level] = 'x265enc pass=cbr bitrate=%(bitrate)d \
-key-int-max=%(max_keyframe_distance)d speed-preset=veryfast tune=zerolatency vbv-buf-capacity=0' %config #option-string="no-sliced-threads"
+            config['video_encoder_desc' + level] = 'x265enc \
+                pass=cbr \
+                bitrate=%(bitrate)d \
+                key-int-max=%(max_keyframe_distance)d \
+                speed-preset=veryfast \
+                tune=zerolatency \
+                vbv-buf-capacity=0' %config #option-string="no-sliced-threads"
     #
     if config.get('text_overlay', ''):
         config['font_size'] = 20
-        s = 'textoverlay font-desc="Mono %(font_size)d" text="%(text_overlay)s" halignment=left valignment=top shaded-background=true xpad=0 ypad=0 auto-resize=false ! ' %config
+        s = '''textoverlay 
+            font-desc="Mono %(font_size)d" \
+            text="%(text_overlay)s" \
+            halignment=left \
+            valignment=top \
+            shaded-background=true \
+            xpad=0 \
+            ypad=0 \
+            auto-resize=false ! ''' %config
         config['video_encoder_desc' + level] = s + config['video_encoder_desc' + level]
     if config.get('time_overlay', ''):
         config['font_size'] = 20
-        s = 'timeoverlay font-desc="Mono %(font_size)d" halignment=right valignment=top shaded-background=true xpad=0 ypad=0 auto-resize=false ! ' %config
+        s = '''timeoverlay \
+            font-desc="Mono %(font_size)d" \
+            halignment=right \
+            valignment=top \
+            shaded-background=true \
+            xpad=0 \
+            ypad=0 \
+            auto-resize=false ! ''' %config
         config['video_encoder_desc' + level] = s + config['video_encoder_desc' + level]
     if config.get('clock_overlay', ''):
         config['font_size'] = config['height'] / 10
-        s = 'textoverlay name=clock_overlay font-desc="Mono %(font_size)d" halignment=left valignment=bottom shaded-background=true xpad=5 ypad=5 auto-resize=false ! ' %config
+        s = '''textoverlay \
+            name=clock_overlay \
+            font-desc="Mono %(font_size)d" \
+            halignment=left \
+            valignment=bottom \
+            shaded-background=true \
+            xpad=5 \
+            ypad=5 \
+            auto-resize=false ! ''' %config
         config['video_encoder_desc' + level] = s + config['video_encoder_desc' + level]
 
 def getProducerPipelineDesc(config):
